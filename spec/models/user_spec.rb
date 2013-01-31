41aa11b463005dc4b57d3cad04c4c1d00c7f9479
Kohei Hasegawa <ameutau@gmail.com>
2013-02-01 02:06:44 +0900

    add tiny spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,40 @@
+# coding: utf-8
+
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".create_with_omniauth" do
+    subject { described_class.create_with_omniauth(auth) }
+
+    context "with valid params" do
+      let(:auth) {
+        {
+          "provider" => "github",
+          "uid"      => "bogus_id",
+          "info" => {
+            "nickname" => "banyan",
+            "email"    => "ameutau@gmail.com"
+          }
+        }
+      }
+
+      it "should create a user" do
+        expect {
+          subject
+        }.to change {
+          User.count
+        }.by(1)
+      end
+    end
+
+    context "with invalid params" do
+      let(:auth) { {} }
+
+      it do
+        expect {
+          subject
+        }.to raise_error
+      end
+    end
+  end
 end
