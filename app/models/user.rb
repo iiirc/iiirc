b583cd6fa6267c5bb085c6402d61a510d2e4b69b
Kohei Hasegawa <ameutau@gmail.com>
2013-02-03 04:47:32 +0900

    organization 関連を user に追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
+  attr_accessible :organization_ids
+
   include Gravtastic
   gravtastic secure: true, size: 24
 
   has_many :snippets, dependent: :destroy
+  has_many :user_organizations, dependent: :destroy
+  has_many :organizations, through: :user_organizations
+  accepts_nested_attributes_for :user_organizations
 
   def self.new_with_omniauth(auth)
     new do |user|
@@ -10,6 +15,7 @@ class User < ActiveRecord::Base
       user.uid      = auth['uid']
       user.username = auth['info']['nickname']
       user.email    = auth['info']['email']
+      user.token    = auth['credentials']['token']
     end
   end
 
