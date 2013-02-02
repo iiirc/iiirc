d7449e9c3ccb64fd017100274b04ef3bab489804
Kohei Hasegawa <ameutau@gmail.com>
2013-02-03 04:48:04 +0900

    controller で organization を取得している...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,20 @@
+# coding: utf-8
+
 class UsersController < ApplicationController
   verify session: :params_from_authenticator, only: %w(new create), redirect_to: :login_path
 
   def new
     @user = User.new_with_omniauth(session[:params_from_authenticator])
+    client = Octokit::Client.new(login: @user.username, oauth_token: @user.token)
+    organizations = client.organizations(@user.username)
+    @organizations = organizations.map do |organization|
+      Organization.find_or_create_by_login_and_original_id(organization.login, organization.id)
+    end
   end
 
   def create
     @user = User.new_with_omniauth(session[:params_from_authenticator])
+    # TODO ここでは厳密にこのユーザが本当に Organization に紐づいているかの検証を GitHub 側に確認する必要がある
     @user.attributes = params[:user]
 
     if @user.save
