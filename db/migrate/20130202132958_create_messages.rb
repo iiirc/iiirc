e9f0bb2364b6bcfffa8564f9088c0bc05fe3d0a3
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-02-02 22:37:07 +0900

    Add CreateMessages migration

--- db/migrate/20130202132958_create_messages.rb
+++ db/migrate/20130202132958_create_messages.rb
@@ -1,11 +1,15 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
+      t.references :snippet
       t.text :raw_content
       t.text :content
       t.string :nick
 
       t.timestamps
     end
+
+    add_index :messages, :snippet_id
+    add_index :messages, :nick
   end
 end
