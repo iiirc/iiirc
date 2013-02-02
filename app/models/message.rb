4272b0cc7793f514efe5b7b0ac150bb2fef207da
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-02-03 03:32:31 +0900

    Message初期化時にnickとcontentを補完する

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,6 +5,19 @@ class Message < ActiveRecord::Base
 
   validates :raw_content, presence: true
 
+  def initialize(*)
+    super
+
+    return if self.raw_content.blank?
+    if self.content.blank? || self.nick.blank?
+      matched = /^(?:\d\d\:\d\d)\s+(?<nick>[^\s\:]+)\: (?<content>.*)$/.match(self.raw_content) # todo: use setting
+      if matched
+        self.content ||= matched[:content]
+        self.nick    ||= matched[:nick]
+      end
+    end
+  end
+
   def to_s
     raw_content
   end
