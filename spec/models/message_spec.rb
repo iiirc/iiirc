74a38f3085f463494aca8dd1633e1fd7faf762a5
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-02-24 02:44:24 +0900

    Specify method name to be tested

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,26 +2,28 @@
 require 'spec_helper'
 
 describe Message do
-  subject { described_class.new }
+  describe '#parse_content' do
+    subject { described_class.new }
 
-  it do
-    content = '16:31 lighty_z: rspecに詳しい方すいません'
-    subject.raw_content = content
-    expect(subject.parse_content).to be_true
-  end
+    it do
+      content = '16:31 lighty_z: rspecに詳しい方すいません'
+      subject.raw_content = content
+      expect(subject.parse_content).to be_true
+    end
 
-  it do
-    content = '16:46 kuroda is now known as kuroda_away'
-    subject.raw_content = content
-    expect(subject.parse_content).to be_nil
-  end
+    it do
+      content = '16:46 kuroda is now known as kuroda_away'
+      subject.raw_content = content
+      expect(subject.parse_content).to be_nil
+    end
 
-  it do
-    content = '16:31 lighty_z: rspecに詳しい方すいません'
-    subject.raw_content = content
-    subject.parse_content
-    expect(subject.time).to eq('16:31')
-    expect(subject.nick).to eq('lighty_z')
-    expect(subject.content).to eq('rspecに詳しい方すいません')
+    it do
+      content = '16:31 lighty_z: rspecに詳しい方すいません'
+      subject.raw_content = content
+      subject.parse_content
+      expect(subject.time).to eq('16:31')
+      expect(subject.nick).to eq('lighty_z')
+      expect(subject.content).to eq('rspecに詳しい方すいません')
+    end
   end
 end
