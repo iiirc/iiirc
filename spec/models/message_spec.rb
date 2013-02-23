26697a7c5b31b0f1e76eb812d789119705916528
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-02-24 02:34:23 +0900

    Add specifications for Message#parse_content

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,27 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe Message do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new }
+
+  it do
+    content = '16:31 lighty_z: rspecに詳しい方すいません'
+    subject.raw_content = content
+    expect(subject.parse_content).to be_true
+  end
+
+  it do
+    content = '16:46 kuroda is now known as kuroda_away'
+    subject.raw_content = content
+    expect(subject.parse_content).to be_nil
+  end
+
+  it do
+    content = '16:31 lighty_z: rspecに詳しい方すいません'
+    subject.raw_content = content
+    subject.parse_content
+    expect(subject.time).to eq('16:31')
+    expect(subject.nick).to eq('lighty_z')
+    expect(subject.content).to eq('rspecに詳しい方すいません')
+  end
 end
