ac106d5fe6912868d045bc020e9034e7769d1ca7
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-02-28 02:28:49 +0900

    Make Win Limechat logs acceptable

--- app/models/message.rb
+++ app/models/message.rb
@@ -10,10 +10,10 @@ class Message < ActiveRecord::Base
 
   def parse_content
     return if self.raw_content.blank?
-    matched = /^(?<time>\d\d\:\d\d)\s+(?<nick>[^\s\:]+)\: (?<content>.*)$/.match(self.raw_content) # todo: use setting
+    matched = /^(?<time>\d\d\:\d\d)\s+(?<nick>([^\s\:]+)\:|(?<nick>\([^\s\)]+\))) (?<content>.*)$/.match(self.raw_content) # todo: use setting
     if matched
       self.content ||= matched[:content]
-      self.nick    ||= matched[:nick]
+      self.nick    ||= matched[:nick].delete(':()')
       self.time    ||= matched[:time]
     end
   end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,28 +2,39 @@
 require 'spec_helper'
 
 describe Message do
-  describe '#parse_content' do
-    subject { described_class.new }
+  subject { described_class.new }
 
+  shared_examples '#parse_content' do
     it do
-      content = '16:31 lighty_z: rspecに詳しい方すいません'
-      subject.raw_content = content
+      subject.raw_content = regular_message
       expect(subject.parse_content).to be_true
     end
 
     it do
-      content = '16:46 kuroda is now known as kuroda_away'
-      subject.raw_content = content
+      subject.raw_content = server_message
       expect(subject.parse_content).to be_nil
     end
 
     it do
-      content = '16:31 lighty_z: rspecに詳しい方すいません'
-      subject.raw_content = content
+      subject.raw_content = regular_message
       subject.parse_content
       expect(subject.time).to eq('16:31')
       expect(subject.nick).to eq('lighty_z')
       expect(subject.content).to eq('rspecに詳しい方すいません')
     end
+  end
+
+  context 'Mac LimeChat log' do
+    let(:regular_message) { '16:31 lighty_z: rspecに詳しい方すいません' }
+    let(:server_message) { '16:46 kuroda is now known as kuroda_away' }
+
+    it_behaves_like '#parse_content'
+  end
+
+  context 'Windows LimeChat log' do
+    let(:regular_message) { '16:31 (lighty_z) rspecに詳しい方すいません' }
+    let(:server_message) { '12:06 *shikakun quit (Ping timeout: 252 seconds)' }
+
+    it_behaves_like '#parse_content'
   end
 end
