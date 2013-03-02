496e6b2fb722a3d4f18f1bfee8abe9f7b535574d
Kohei Hasegawa <ameutau@gmail.com>
2013-03-03 07:11:30 +0900

    add comment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  # GET /auth/:provider/callback
   def callback
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth[:provider], auth[:uid])
@@ -12,13 +13,13 @@ class SessionsController < ApplicationController
     end
   end
 
+  # DELETE /signout
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, :notice => "Signed out!"
+    redirect_to root_path, :notice => "Successfully signed out."
   end
 
   private
-
   def store_auth_params(auth)
     paramz               = auth.slice(:provider, :uid)
     paramz[:info]        = auth[:info].symbolize_keys.slice(:email, :nickname) # nickname is like -> "banyan"
