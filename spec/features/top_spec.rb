e2e3db2d5f03bd4e8547fc49ab8fe4641f80cfc5
Kohei Hasegawa <ameutau@gmail.com>
2013-03-03 05:51:15 +0900

    add feature spec for top

---
+++ spec/features/top_spec.rb
@@ -0,0 +1,14 @@
+# coding: utf-8
+require 'spec_helper'
+
+describe "Top" do
+  describe "GET /" do
+    subject { page }
+
+    it "should render" do
+      visit root_path
+
+      expect(page.status_code).to eq(200)
+    end
+  end
+end
