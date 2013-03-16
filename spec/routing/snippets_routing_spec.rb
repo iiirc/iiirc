e31bfefaaac9ffa39943ab25b73a8386667aa4f5
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-03-17 01:09:01 +0900

    Use new notation of RSpec

--- spec/routing/snippets_routing_spec.rb
+++ spec/routing/snippets_routing_spec.rb
@@ -4,31 +4,31 @@ describe SnippetsController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/snippets").should route_to("snippets#index")
+      expect(get("/snippets")).to route_to("snippets#index")
     end
 
     it "routes to #new" do
-      get("/snippets/new").should route_to("snippets#new")
+      expect(get("/snippets/new")).to route_to("snippets#new")
     end
 
     it "routes to #show" do
-      get("/snippets/1").should route_to("snippets#show", :id => "1")
+      expect(get("/snippets/1")).to route_to("snippets#show", :id => "1")
     end
 
     it "routes to #edit" do
-      get("/snippets/1/edit").should route_to("snippets#edit", :id => "1")
+      expect(get("/snippets/1/edit")).to route_to("snippets#edit", :id => "1")
     end
 
     it "routes to #create" do
-      post("/snippets").should route_to("snippets#create")
+      expect(post("/snippets")).to route_to("snippets#create")
     end
 
     it "routes to #update" do
-      put("/snippets/1").should route_to("snippets#update", :id => "1")
+      expect(put("/snippets/1")).to route_to("snippets#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      delete("/snippets/1").should route_to("snippets#destroy", :id => "1")
+      expect(delete("/snippets/1")).to route_to("snippets#destroy", :id => "1")
     end
 
   end
