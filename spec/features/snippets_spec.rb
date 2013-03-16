2da82669668c1b57cfc743a37ff594f229593968
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-03-17 01:24:33 +0900

    Add a feature spec for /snippets/1

--- spec/features/snippets_spec.rb
+++ spec/features/snippets_spec.rb
@@ -9,4 +9,13 @@ describe "Snippets" do
       expect(page.status_code).to be(200)
     end
   end
+
+  describe 'GET /snippets/1' do
+    subject { page }
+
+    it 'should not render' do
+      visit snippet_path id: 1
+      expect(page.status_code).to be(404)
+    end
+  end
 end
