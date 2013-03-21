6ce16bffca0e46d98e7ef5344850d1d3e3b57785
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-03-22 01:06:52 +0900

    Add features specs for users around unpublished snippets

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -26,6 +26,24 @@ describe 'Users' do
       it "show user's snippets" do
         expect(page).to have_css("#snippet_#{snippet.id}")
       end
+
+      it "don't show unpublished snippets" do
+        snippet.update_column :published, false
+        visit user_path(user.username)
+        expect(page).not_to have_css('[id~="snippet_"]')
+      end
+
+      context 'signed in as the user' do
+        before do
+          sign_in(user)
+          snippet.update_column :published, false
+        end
+
+        it "show unpublished snippets" do
+          visit user_path(user.username)
+          expect(page).to have_css("#snippet_#{snippet.id}")
+        end
+      end
     end
   end
 end
