75b160df16684e2f997f3e2cf6d5082a01ce3dae
Kohei Hasegawa <ameutau@gmail.com>
2013-04-20 03:25:10 +0900

    oops.......

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -2,7 +2,7 @@ require 'twitter'
 
 if Rails.env.production?
   Twitter.configure do |config|
-    config.consumer_key       = Settings.twitter.client_id
+    config.consumer_key       = Settings.twitter.consumer_key
     config.consumer_secret    = Settings.twitter.consumer_secret
     config.oauth_token        = Settings.twitter.oauth_token
     config.oauth_token_secret = Settings.twitter.oauth_token_secret
