25a5a6fba003179f72366e7a589d2018ebfeb9c9
Kohei Hasegawa <ameutau@gmail.com>
2013-04-20 17:55:40 +0900

    deletable snippet only if snippet owner

--- spec/features/snippets_spec.rb
+++ spec/features/snippets_spec.rb
@@ -17,4 +17,28 @@ describe "Snippets" do
       expect(page).to have_content snippet.title
     end
   end
+
+  describe "DELETE /snippet/1", js: true do
+    let!(:user)    { Fabricate(:user) }
+    let!(:snippet) { Fabricate(:snippet, user: user) }
+
+    context "when user is not logged in" do
+      it "should not show delete button" do
+        visit snippet_path(snippet.id)
+        expect(page).to_not have_content "Delete Snippet!"
+      end
+    end
+
+    context "when user is logged in" do
+      before { sign_in(user) }
+
+      it "should show delete button and delete snippet" do
+        visit snippet_path(snippet.id)
+        expect(page).to have_content "Delete Snippet!"
+        click_on "Delete Snippet!"
+        expect(page).to have_content "Snippet was successfully deleted."
+        expect(current_path).to be == root_path
+      end
+    end
+  end
 end
