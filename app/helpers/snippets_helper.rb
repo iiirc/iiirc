67c4a0a0902477e0b577d014380e2467f2a4182b
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-04-21 03:29:07 +0900

    Deny User object argument for SnippetsHelper#github_user_url

--- app/helpers/snippets_helper.rb
+++ app/helpers/snippets_helper.rb
@@ -1,6 +1,5 @@
 module SnippetsHelper
-  def github_user_url(user)
-    username = user.respond_to?(:username) ? user.username : user
+  def github_user_url(username)
     "https://github.com/#{ERB::Util.url_encode(username)}"
   end
 end
