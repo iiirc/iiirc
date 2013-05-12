10ec63acc5062604dbeeea829ff2833faca236c0
Kohei Hasegawa <ameutau@gmail.com>
2013-05-13 01:46:44 +0900

    add spec
    test for coverall

--- spec/features/organizations_spec.rb
+++ spec/features/organizations_spec.rb
@@ -10,4 +10,15 @@ describe "Organizations" do
       expect(page.status_code).to be == 200
     end
   end
+
+  describe "GET /organizations/paperboy-all" do
+    let(:organization) { Fabricate(:organization, login: "paperboy-all") }
+
+    subject { page }
+
+    it "should render" do
+      visit organization_path(organization.login)
+      expect(page.status_code).to be == 200
+    end
+  end
 end
