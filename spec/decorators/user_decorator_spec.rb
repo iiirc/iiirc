4567cb6a544086b65ed64643d81547ba075e4837
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-05-16 02:58:34 +0900

    Add test for UserDecorator

--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe UserDecorator do
+  describe '#github_url' do
+    let(:user) { Fabricate(:user).decorate }
+
+    it 'should return user URI on GitHub' do
+      expect(user.github_url).to eq('https://github.com/alice0')
+    end
+  end
 end
