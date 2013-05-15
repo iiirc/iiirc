a73f5f3a58b9435f33d6beb31a8e8844021bc100
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-05-15 18:32:25 +0900

    Add o switch for performance

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,7 @@ class Message < ActiveRecord::Base
   REGEXP = /
     ^(?<time>\d\d\:\d\d)\s+(?<nick>([^\s\:]+)\:|(?<nick>\([^\s\)]+\)))\ (?<content>.*)$|
     ^(?<time>\d{4}-\d{2}-\d{2}\ \d{2}:\d{2}:\d{2})\t(?<nick>([^\s]+))\t(?<content>.*)$
-  /x
+  /xo
 
   belongs_to :snippet
   has_many :stars, dependent: :destroy
