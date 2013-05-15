5263bf1ef3f188573cc827b2ec6c36b568d06761
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-05-15 18:27:03 +0900

    [REFACTORING]Use a constant for Regexp instead of hard coding

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,9 @@
 class Message < ActiveRecord::Base
+  REGEXP = /
+    ^(?<time>\d\d\:\d\d)\s+(?<nick>([^\s\:]+)\:|(?<nick>\([^\s\)]+\)))\ (?<content>.*)$|
+    ^(?<time>\d{4}-\d{2}-\d{2}\ \d{2}:\d{2}:\d{2})\t(?<nick>([^\s]+))\t(?<content>.*)$
+  /x
+
   belongs_to :snippet
   has_many :stars, dependent: :destroy
 
@@ -10,8 +15,7 @@ class Message < ActiveRecord::Base
 
   def parse_content
     return if self.raw_content.blank?
-    matched = /^(?<time>\d\d\:\d\d)\s+(?<nick>([^\s\:]+)\:|(?<nick>\([^\s\)]+\))) (?<content>.*)$/.match(self.raw_content) # todo: use setting
-    matched = /^(?<time>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})\t(?<nick>([^\s]+))\t(?<content>.*)$/.match(self.raw_content) unless matched
+    matched = REGEXP.match(self.raw_content)
     if matched
       self.content ||= matched[:content]
       self.nick    ||= matched[:nick].delete(':()')
