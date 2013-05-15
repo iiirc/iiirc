bd6c5c3be41737232c0f7513f2bc9dcdfa722086
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-05-16 04:03:28 +0900

    Add test for message including image URI

--- spec/decorators/message_decorator_spec.rb
+++ spec/decorators/message_decorator_spec.rb
@@ -7,5 +7,13 @@ describe MessageDecorator do
     it 'marks URIs up' do
       expect(message.content).to eq('<a href="http://iiirc.org/">http://iiirc.org/</a>')
     end
+
+    context 'when including image URI' do
+      let(:message) { Message.new(content: 'http://iiirc.org/assets/iiirc.gif').decorate }
+
+      it 'marks URIs up with a and img tag' do
+        expect(message.content).to eq('<a href="http://iiirc.org/assets/iiirc.gif">http://iiirc.org/assets/iiirc.gif<br><img src="http://iiirc.org/assets/iiirc.gif" alt=""></a>')
+      end
+    end
   end
 end
