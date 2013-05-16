1007b1534382e293eebb399e8d859c96bbdc6b9a
Kohei Hasegawa <ameutau@gmail.com>
2013-05-16 12:40:32 +0900

    add controller spec for star controller
    @KitaitiMakoto
    
    i told you i don wanna use controller spec,
    but in this case, we need to use controller spec instead of request spec.
    do you agree?

---
+++ spec/controllers/stars_controller_spec.rb
@@ -0,0 +1,68 @@
+# coding: utf-8
+require 'spec_helper'
+
+describe StarsController do
+  let(:user)    { Fabricate(:user)    }
+  let(:message) { Fabricate(:message) }
+
+  before do
+    controller.stub(:current_user) { user }
+    request.env["HTTP_ACCEPT"] = 'application/json'
+  end
+
+  def valid_attributes
+    { message_id: message.id }
+  end
+
+  describe "POST create" do
+    context "when star record is not found" do
+      it "should return http success" do
+        post :create, valid_attributes
+        expect(response.status).to eq 200
+      end
+
+      it "should return json response" do
+        post :create, valid_attributes
+        expect(ActiveSupport::JSON.decode(response.body)).to_not be_nil
+      end
+
+      it "count should be 1 in json response" do
+        post :create, valid_attributes
+        expect(ActiveSupport::JSON.decode(response.body)["count"]).to eq 1
+      end
+
+      it "should create star record" do
+        expect {
+          post :create, valid_attributes
+        }.to change { Star.count }.by(1)
+      end
+    end
+
+    context "when star record is found" do
+      before do
+        Fabricate(:star, user: user, message: message)
+      end
+
+      it "should return http success" do
+        post :create, valid_attributes
+        expect(response.status).to eq 200
+      end
+
+      it "should return json response" do
+        post :create, valid_attributes
+        expect(ActiveSupport::JSON.decode(response.body)).to_not be_nil
+      end
+
+      it "count should be 2 in json response" do
+        post :create, valid_attributes
+        expect(ActiveSupport::JSON.decode(response.body)["count"]).to eq 2
+      end
+
+      it "should not create star record" do
+        expect {
+          post :create, valid_attributes
+        }.to change { Star.count }.by(0)
+      end
+    end
+  end
+end
