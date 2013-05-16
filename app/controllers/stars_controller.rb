9bf179d4cb9450990198506a2e935edd48bad47c
Kohei Hasegawa <ameutau@gmail.com>
2013-05-16 12:42:17 +0900

    [REFACTOR] use find_or_create_by_user_id_and_message_id

--- app/controllers/stars_controller.rb
+++ app/controllers/stars_controller.rb
@@ -2,14 +2,7 @@ class StarsController < ApplicationController
   # @todo use transaction or 'INSERT OR UPDATE'
   def create
     message = Message.find(params[:message_id])
-    star = Star.find_by_user_id_and_message_id(current_user.id, message.id)
-    if star
-      star.count += 1
-      Rails.logger.debug 'exists'
-    else
-      star = Star.new(user_id: current_user.id, count: 1)
-      Rails.logger.debug 'not exists'
-    end
+    star = Star.find_or_create_by_user_id_and_message_id(current_user.id, message.id)
 
     respond_to do |format|
       if message.stars << star
