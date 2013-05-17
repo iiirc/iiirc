89898e4479a6d9ffb84d055456b386e27f597a33
Kohei Hasegawa <ameutau@gmail.com>
2013-05-18 02:25:13 +0900

    set publicity
    Public post! という文言に頼ってるので危険ではある...
    もし事故があっても基本 Secret になってしまうほうが、
    Secret が Public になってしまうよりは事故が少ないでそちらに寄せた

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -45,7 +45,7 @@ class SnippetsController < ApplicationController
     return render_access_denied if current_user.blank?
     content = params[:content].strip
     snippet = current_user.snippets.build(params[:snippet])
-    snippet.published = params[:commit] != "Secret post!"  # あとでもうちょっとちゃんとします...
+    snippet.published = params[:commit] == "Public post!"
 
     content.each_line.collect do |raw_content|
       snippet.messages.build(raw_content: raw_content.chomp)
