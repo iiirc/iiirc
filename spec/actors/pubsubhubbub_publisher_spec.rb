3dbc8f9d0100795927cb545955300d06d3f70fd3
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-05-20 07:26:10 +0900

    Extract method to create a snippet

--- spec/actors/pubsubhubbub_publisher_spec.rb
+++ spec/actors/pubsubhubbub_publisher_spec.rb
@@ -11,18 +11,19 @@ describe PubsubhubbubPublisher do
 
   it 'post to hub on creating snippet' do
     PubsubhubbubPublisher.should_receive(:perform)
-
-    user = Fabricate(:user)
-    snippet = user.snippets.build(published: true)
-    snippet.messages.build raw_content: 'raw content'
-    snippet.save
+    create_snippet published: true
   end
 
   it "don't post to hub on creating secret snippet" do
     PubsubhubbubPublisher.should_not_receive(:perform)
+    create_snippet published: false
+  end
 
+  private
+
+  def create_snippet(params={})
     user = Fabricate(:user)
-    snippet = user.snippets.build(published: false)
+    snippet = user.snippets.build(params)
     snippet.messages.build raw_content: 'raw content'
     snippet.save
   end
