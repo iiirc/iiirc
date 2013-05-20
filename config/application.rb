85fdae3fd3a48c61a9ebba5ab130d0c8fa813e6a
Kohei Hasegawa <ameutau@gmail.com>
2013-05-21 01:57:52 +0900

    rails4 assets enabled is true by default, so remove it

--- config/application.rb
+++ config/application.rb
@@ -48,9 +48,6 @@ module Iiirc
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = false
 
-    # Enable the asset pipeline
-    config.assets.enabled = true
-
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
