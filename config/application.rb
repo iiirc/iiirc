a290ad4beff5e0008dd4e65c23d7fd15955b7af4
Kohei Hasegawa <ameutau@gmail.com>
2013-05-21 01:14:30 +0900

    simpifys config file

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,7 @@ require "active_resource/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
+Bundler.require(:default, Rails.env)
 
 module Iiirc
   class Application < Rails::Application
