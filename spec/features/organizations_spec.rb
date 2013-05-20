bd58d1778bbe4e1f8489d074038b1a69f4d8e56f
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-05-20 12:08:29 +0900

    Add tests for organization pages

--- spec/features/organizations_spec.rb
+++ spec/features/organizations_spec.rb
@@ -20,5 +20,41 @@ describe "Organizations" do
       visit organization_path(organization.login)
       expect(page.status_code).to be == 200
     end
+
+    context 'when organization has mebers' do
+      let(:user) { Fabricate(:user) }
+      before do
+        organization.users << user
+      end
+
+      it 'should render link to user page of members' do
+        visit organization_path(organization.login)
+        expect(page).to have_link(user.username)
+      end
+    end
+
+    describe 'snippets' do
+      let(:user) { Fabricate(:user) }
+      let!(:snippet) {
+        Fabricate(:snippet, user: user, organization: organization) do
+          before_validation do |snippet|
+            snippet.messages << Fabricate(:message)
+          end
+        end
+      }
+
+      it 'render snippets related to organization' do
+        visit organization_path(organization.login)
+
+        expect(page).to have_css("#snippet_#{snippet.id}")
+      end
+
+      it "don't render secret snippets" do
+        snippet.update_column :published, false
+        visit organization_path(organization.login)
+
+        expect(page).not_to have_css("#snippet_#{snippet.id}")
+      end
+    end
   end
 end
