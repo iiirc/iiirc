5a5c2fdf9f340c5ab2ade3628114b65bd92544da
Kohei Hasegawa <ameutau@gmail.com>
2013-05-20 12:57:02 +0900

    adapt linyows weechat log format

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,9 @@ class Message < ActiveRecord::Base
   REGEXP = /
     ^(?<time>\d\d\:\d\d)\s+(?<nick>([^\s\:]+)\:|(?<nick>\([^\s\)]+\)))\ (?<content>.*)$
     |
-    ^(?<time>\d{4}-\d{2}-\d{2}\ \d{2}:\d{2}:\d{2})\t(?<nick>([^\s]+))\t(?<content>.*)$ # Weechat
+    ^(?<time>\d{4}-\d{2}-\d{2}\ \d{2}:\d{2}:\d{2})\t(?<nick>([^\s]+))\t(?<content>.*)$ # Weechat1
+    |
+    ^(?<time>\d{2}:\d{2}:\d{2})\ (?<nick>([^\s]+))\ \|\ (?<content>.*)$ # Weechat2
     |
     ^\[(?<time>\d{2}:\d{2}:\d{2})\]\ (?<nick>[^:]+):\ (?<content>(.*))$ # Textual
     |
