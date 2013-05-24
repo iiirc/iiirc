2618d712f109027442edcf45ab8631a2741d632a
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-05-24 14:00:35 +0900

    ChangeEmailToUsers have not been downable

--- db/migrate/20130524042610_change_email_to_users.rb
+++ db/migrate/20130524042610_change_email_to_users.rb
@@ -1,5 +1,13 @@
 class ChangeEmailToUsers < ActiveRecord::Migration
-  def change
+  def up
     change_column :users, :email, :string, :null => true
+  end
+
+  def down
+    User.where(email: nil).each do |user|
+      user.email = ''
+      user.save!
+    end
+    change_column :users, :email, :string, :null => false
   end
 end
