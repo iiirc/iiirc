a222289d0dbfcbb6d0e66584335b2d2efb9307ce
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-05-25 16:54:35 +0900

    Calcurate cache key for each snippet in decorator

--- app/decorators/snippet_decorator.rb
+++ app/decorators/snippet_decorator.rb
@@ -2,4 +2,8 @@ class SnippetDecorator < Draper::Decorator
   delegate_all
   decorates_association :user
   decorates_association :messages
+
+  def cache_key(options={format: :html})
+    "snippet_#{model.id}.#{options[:format]}"
+  end
 end
