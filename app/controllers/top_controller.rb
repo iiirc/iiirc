c0e529979a4e5b92bc6c8722381ec2fca6fd925c
Kohei Hasegawa <ameutau@gmail.com>
2013-05-26 02:23:00 +0900

    fix deprecation warning -  Relation#all is deprecated.

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def index
-    @organizations = Organization.has_snippets.all
+    @organizations = Organization.has_snippets
     @snippets = Snippet.with_assoc.published.date_desc.page(params[:page]).decorate
 
     respond_to do |format|
