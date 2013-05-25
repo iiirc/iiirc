45b76e4b1d83e07c8a6ebfb16bcfa33dd5a7e67d
Kohei Hasegawa <ameutau@gmail.com>
2013-05-26 02:15:17 +0900

    Add scope

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,6 +3,8 @@ class Organization < ActiveRecord::Base
   has_many :users, through: :user_organizations
   has_many :snippets
 
+  scope :has_snippets, -> { joins(:snippets) }
+
   def to_param
     login
   end
