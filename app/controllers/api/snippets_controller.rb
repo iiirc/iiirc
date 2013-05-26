54be1d23a6b9b26340789be94c420ee2227f632e
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-05-27 06:08:03 +0900

    Hide stars in snippets provides by API

--- app/controllers/api/snippets_controller.rb
+++ app/controllers/api/snippets_controller.rb
@@ -1,11 +1,11 @@
 class Api::SnippetsController < ApplicationController
   def show
-    snippet = Snippet.with_assoc.find_by_hash_id(params[:id])
-    if snippet.blank?
-      snippet = Snippet.with_assoc.find_by_id(params[:id])
-      return render_not_found unless snippet.try(:published?)
+    @snippet = Snippet.with_assoc.find_by_hash_id(params[:id])
+    if @snippet.blank?
+      @snippet = Snippet.with_assoc.find_by_id(params[:id])
+      return render_not_found unless @snippet.try(:published?)
     end
-    @html = render_to_string(partial: 'snippet', layout: false, locals: {snippet: snippet.decorate, current_user: nil})
+    @html = render_to_string(partial: 'snippet', layout: false, locals: {snippet: @snippet.decorate, current_user: nil})
 
     respond_to do |format|
       format.js
