5301f862796ef264e16a4254b7fadb0efa41bb50
KITAITI Makoto <KitaitiMakoto@gmail.com>
2013-12-25 22:08:17 +0900

    Fix test

--- spec/controllers/snippets_controller_spec.rb
+++ spec/controllers/snippets_controller_spec.rb
@@ -2,20 +2,17 @@
 require 'spec_helper'
 
 describe SnippetsController do
-  let(:user) { Fabricate(:user)    }
+  let(:user) { Fabricate(:user) }
   describe "POST preview" do
     before do
       allow(controller).to receive(:current_user) { user }
     end
 
-    it 'respond with JSON of parsed messages' do
-      post :preview, { content: "00:52 shikakun: すごい！" }
+    it 'respond with HTML of parsed messages' do
+      post :preview, { snippet: {title: ''}, content: "00:52 shikakun: すごい！" }
 
       expect(response.status).to eq 200
-      expect(ActiveSupport::JSON.decode(response.body)).to eq({
-        'nick_offset' => 169895279847015425395522194364081377003,
-        'messages' => [{'raw_content' => '00:52 shikakun: すごい！', 'time' => '00:52', 'nick' => 'shikakun', 'content' => 'すごい！'}]
-      })
+      expect(response.body).to have_content '00:52'
     end
   end
 end
